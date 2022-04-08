736e7a909d7113c62751ea2a99be8994189c3b2e
Documenter.jl <documenter@juliadocs.github.io>
2022-04-08 22:30:26 +0000

    build based on 0b9cb5f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FiniteSizeScaling","category":"page"},{"location":"#FiniteSizeScaling","page":"Home","title":"FiniteSizeScaling","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FiniteSizeScaling.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [FiniteSizeScaling]","category":"page"}]
+}
