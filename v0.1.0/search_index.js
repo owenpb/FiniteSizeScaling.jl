120007da8d65fdf4748deba35b3ddccccf364c0f
Documenter.jl <documenter@juliadocs.github.io>
2022-04-09 01:11:59 +0000

    build based on 2aa8b0d

---
+++ v0.1.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = FiniteSizeScaling","category":"page"},{"location":"#FiniteSizeScaling","page":"Home","title":"FiniteSizeScaling","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FiniteSizeScaling: a Julia package for finding optimized values of critical exponents and phase transition temperatures given data for finite-size lattices.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [FiniteSizeScaling]","category":"page"}]
+}
